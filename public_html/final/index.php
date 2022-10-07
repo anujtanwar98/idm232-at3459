b0245d6a6bf76daa20bec01651b4e7808cfd4fe7
Anuj <anujtanwar89@gmail.com>
2022-10-06 20:47:48 -0400

    Assignment 1

--- public_html/final/index.php
+++ public_html/final/index.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php include_once 'components/header.php'; ?>
+<?php $title = 'Assignment - 01';?>
+<h1><?php echo $title; ?></h1>
+<?php include_once 'components/footer.php'; ?>

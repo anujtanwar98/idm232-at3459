c57f7a89baddbe10e5e99b35b545bb3d13cb5424
Anuj <anujtanwar89@gmail.com>
2022-11-21 22:41:20 -0500

    :fire: :coffin: delete extra image

--- public_html/final/_components/header.php
+++ public_html/final/_components/header.php
@@ -5,7 +5,7 @@ if (!isset($page_title)) {
     $page_title = '🚨 Missing Title 🚨';
 }
 $site_title = 'UXID Quick Recipes';
-$document_title = $page_title . ' | ' . $site_title; // Home | JAWN Clips
+$document_title = $page_title . ' | ' . $site_title; // Home | UXID Quick Recipes
 
 ?>
 <!DOCTYPE html>
@@ -18,7 +18,6 @@ $document_title = $page_title . ' | ' . $site_title; // Home | JAWN Clips
   <link rel="icon" type="image/x-icon" href="<?php echo site_url(); ?>/dist/images/anuj_logo.png">
   <link rel="stylesheet" href="<?php echo site_url(); ?>/dist/styles/style.css">
   <title><?php echo $document_title ; ?></title>
-  <link rel="stylesheet" href="./style/styles.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   <script src="https://cdn.tiny.cloud/1/7rpnj47it93x8jhvzo8vbgdulo0j4qdj5xdmud6xc46gy8fb/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 </head>
--- public_html/final/index.php
+++ public_html/final/index.php
@@ -19,75 +19,18 @@ $recipes = get_recipes();
   <a class="text-white" href="<?php echo site_url(); ?>/admin/recipes/create.php" style="text-decoration: none;">Add Recipe</a>
 </button> -->
   <div id="main-my-box">
-    <!-- <div class="p-lg-0 mt-3 mx-0">   -->
-      <!-- <div class="w-100 d-flex flex-wrap justify-content-around justify-content-lg-between m-0"> -->
         <?php include __DIR__ . '/_components/recipesCard.php'; ?>
-      <!-- </div> -->
-    <!-- </div> -->
-    <div class="small-box-recipes">
-      <a class="box-recipes-link" href="./mediterranean-chickpea-salad.php">
+
+        
+<!-- reference to do for cards -->
+    <!-- <div class="small-box-recipes">
+      <a class="box-recipes-link" href="./mediterranean-chickpea-salad.php"> -->
       <!-- <div class="box-image-text"> -->
-        <img src="./dist/images/1.jpg" class="my-images" alt="...">
-        <h4 class="text-center">Mediterranean Chickpea Salad</h4>
+        <!-- <img src="./dist/images/1.jpg" class="my-images" alt="...">
+        <h4 class="text-center">Mediterranean Chickpea Salad</h4> -->
       <!-- </div> -->
-      </a>
-    </div>
-    <div class="small-box-recipes">
-      <a class="box-recipes-link" href="./chickpea-salad-sandwich.php">
-      <!-- <div class="box-image-text"> -->
-        <img src="./dist/images/2.jpg" class="my-images" alt="...">
-        <h4 class="text-center">Chickpea Salad Sandwich</h4>
-      <!-- </div> -->
-      </a>
-    </div>
-    <div class="small-box-recipes">
-      <a class="box-recipes-link" href="./cajun-spiced.php">
-      <!-- <div class="box-image-text"> -->
-        <img src="./dist/images/3.jpg" class="my-images" alt="...">
-        <h4 class="text-center">Cajun-Spiced Grilled Corn</h4>
-      <!-- </div> -->
-      </a>
-    </div>
-    <div class="small-box-recipes">
-      <a class="box-recipes-link" href="./sesame-noodles.php">
-      <!-- <div class="box-image-text"> -->
-        <img src="./dist/images/4.jpg" class="my-images" alt="...">
-        <h4 class="text-center">Sesame Peanut Noodles</h4>
-      <!-- </div> -->
-      </a>
-    </div>
-    <div class="small-box-recipes">
-      <a class="box-recipes-link" href="./vegan-caesar-dressing.php">
-      <!-- <div class="box-image-text"> -->
-        <img src="./dist/images/5.jpg" class="my-images" alt="...">
-        <h4 class="text-center">Vegan Caesar Dressing</h4>
-      <!-- </div> -->
-      </a>
-    </div>
-    <div class="small-box-recipes">
-      <a class="box-recipes-link" href="./spinach-tofu.php">
-      <!-- <div class="box-image-text"> -->
-        <img src="./dist/images/6.jpg" class="my-images" alt="...">
-        <h4 class="text-center">Spinach Tofu Scramble</h4>
-      <!-- </div> -->
-      </a>
-    </div>
-    <div class="small-box-recipes">
-      <a class="box-recipes-link" href="./homemade-tortilla.php">
-      <!-- <div class="box-image-text"> -->
-        <img src="./dist/images/7.jpg" class="my-images" alt="...">
-        <h4 class="text-center">Homemade Tortilla Chips</h4>
-      <!-- </div> -->
-      </a>
-    </div>
-    <div class="small-box-recipes">
-      <a class="box-recipes-link" href="./homemade-pancake.php">
-      <!-- <div class="box-image-text"> -->
-        <img src="./dist/images/8.jpg" class="my-images" alt="...">
-        <h4 class="text-center">Homemade Pancake</h4>
-      <!-- </div> -->
-      </a>
-    </div>
+      <!-- </a>
+    </div> -->
 </div>
 </main>
 

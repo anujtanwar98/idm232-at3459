8bd33ea297dcf2d0c6fd43d050a8f2d9e9fe13bc
Anuj <anujtanwar89@gmail.com>
2022-11-30 14:12:21 -0500

    added view :sparkles: :fire: :rocket:

--- public_html/final/_components/table-recipes.php
+++ public_html/final/_components/table-recipes.php
@@ -11,26 +11,19 @@ if (!isset($recipes)) {
         <th scope="col">Title</th>
         <th scope="col">Small Image</th>
         <th scope="col">Big Image</th>
-        <th scope="col">Ingredients</th>
-        <th scope="col">Steps</th>
+        <!-- <th scope="col">Ingredients</th>
+        <th scope="col">Steps</th> -->
         <th scope="col">
           <span class="sr-only">Edit</span>
         </th>
+        <th scope="col">Actions</th>
       </tr>
     </thead>
 
-  <!-- <table class="min-w-full divide-y divide-gray-300">
-    <thead class="bg-gray-50">
-      <tr>
-        <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">ID</th>
-        <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Title</th>
-        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Ingredients</th>
-        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Steps</th>
-        <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
-          <span class="sr-only">Edit</span>
-        </th>
-      </tr>
-    </thead> -->
+    <!--  this is for ref -->
+    <!-- <td>{$recipe['ingredients']}</td>
+    <td>{$recipe['steps']}</td> -->
+    
     <tbody>
       <?php
       // Cant combine functions with string so we have to assign the function to a variable here.
@@ -42,11 +35,19 @@ if (!isset($recipes)) {
               <td>{$recipe['recipe_title']}</td>
               <td><img src='{$site_url}{$recipe['image_path_small']}' alt=''></td>
               <td><img src='{$site_url}{$recipe['image_path_large']}' alt=''></td>
-              <td>{$recipe['ingredients']}</td>
-              <td>{$recipe['steps']}</td>
+              
               <td>
-                <a href='{$site_url}/admin/recipes/edit.php?id={$recipe['id']}'>Edit</a>
-                <a href='{$site_url}/admin/recipes/delete.php?id={$recipe['id']}'>Delete</a>
+              <button class='btn btn-primary'>
+                <a class='text-white' href='{$site_url}/admin/recipes/edit.php?id={$recipe['id']}' style='text-decoration: none;'>Edit</a>
+              </button>
+              <button class='btn btn-danger'>
+                <a class='text-white' href='{$site_url}/admin/recipes/delete.php?id={$recipe['id']}' style='text-decoration: none;'>Delete</a>
+              </button>
+              </td>
+              <td>
+              <button class='btn btn-primary'>
+                <a class='text-white' href='{$site_url}/recipes-show.php?id={$recipe['id']}' style='text-decoration: none;'>View</a>
+              </button>
               </td>
             </tr>";
       }
--- public_html/final/admin/search/index.php
+++ public_html/final/admin/search/index.php
@@ -27,19 +27,14 @@ if ($results->num_rows > 0) {
 }
 
 ?>
-
-<div class="mx-auto my-16 max-w-7xl px-4">
-  <div class="px-4 sm:px-6 lg:px-8">
+<main class="main">
+<div>
+  <div>
     <!-- <?php include __DIR__ . '/../../_components/navigation-admin.php'; ?> -->
-    <div class="sm:flex sm:items-center">
-      <div class="sm:flex-auto">
-        <h1 class="text-xl font-semibold text-gray-900">Search Results</h1>
-        <!-- <form action="<?php echo site_url(); ?>/admin/search" method="GET">
-          <input class=" border-black border-2" type="text" name="search" id="search" placeholder="Search"
-            value="<?php echo $search; ?>">
-          <button type="submit">Search</button>
-        </form> -->
-        <h2>You searched for "<?php echo $search; ?>"</h2>
+    <div>
+      <div>
+        <h1 class="text-white text-center">Search Results</h1>
+        <h2 class="text-white text-center">You searched for "<?php echo $search; ?>"</h2>
         <?php
         // If no results, echo no results
         if (!$recipes_results) {
@@ -49,23 +44,38 @@ if ($results->num_rows > 0) {
         <?php
 // If error query param exist, show error message
   if (isset($_GET['error'])) {
-      echo '<p class="text-red-500">' . $_GET['error'] . '</p>';
+      echo '<p>' . $_GET['error'] . '</p>';
   }?>
       </div>
-      <!-- <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-        <button type="button"
-          class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
-          <a href="<?php echo site_url() . '/admin/services/create.php' ?>">
-            Add service</a></button>
-      </div> -->
     </div>
 
     <?php
     // If we have results, show them
       if ($recipes_results) {
           while ($recipes_results = mysqli_fetch_assoc($results)) {
-              echo '<div class="flex flex-row justify-center items-center">';
-              echo '<h2>' . $recipes_results['recipe_title'] . '</h2>';
+             echo '<div id="main-my-box">';
+              echo 
+              
+              
+              '<div class="small-box-recipes">' 
+              
+              
+
+              . '<a class="box-recipes-link" href="' . site_url() . '/recipes-show.php?id=' . $recipes_results['id'] . '">'
+              
+              . '<div class="box-image-text">'
+
+              . '<img src="' . site_url() . $recipes_results['image_path_small'] . '" class="my-images" alt="...">'
+
+              . '<h4 class="text-center">' . $recipes_results['recipe_title'] . 
+              
+              '</h4>' . 
+
+              '</div>' .
+
+              '</a>' .
+              
+              '</div>';
               // echo '<h2>' . $recipes_results['recipe_title'] . ' ' . $recipes_results['ingredients'] . ' ' . $recipes_results['steps'] . '</h2>';
               echo '</div>';
           }
@@ -74,8 +84,7 @@ if ($results->num_rows > 0) {
 
   </div>
 </div>
-
-
+</main>
 
 <?php include_once __DIR__ . '/../../_components/footer.php';
 ?>
